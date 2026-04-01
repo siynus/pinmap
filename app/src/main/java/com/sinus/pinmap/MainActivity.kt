b1ccae4ea16bcde95e387d2410028f0dc56fe9aa
sinus <1075890116@qq.com>
2026-04-01 10:59:23 +0800

    init repo

---
+++ app/src/main/java/com/sinus/pinmap/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.sinus.pinmap
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sinus.pinmap.ui.theme.PinmapTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.layout_main)
+    }
+}
