33e7fcd68194dc8ec6faecbaa92f11f2da97700a
sinus <1075890116@qq.com>
2026-07-14 01:07:58 +0800

    fix: integrate secrets-gradle-plugin for API key management

--- app/src/main/java/com/sinus/pinmap/PinmapApplication.kt
+++ app/src/main/java/com/sinus/pinmap/PinmapApplication.kt
@@ -7,9 +7,8 @@ class PinmapApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
+        MapsInitializer.setApiKey(BuildConfig.MAPS_API_KEY)
         System.setProperty("amap.sdk.update.enable", "false")
-
         try {
             MapsInitializer.initialize(this)
         } catch (e: Exception) {
