2a997b03348aa4b00585536180ec35c8815e21ee
sinus <1075890116@qq.com>
2026-07-16 22:56:43 +0800

    Revert "feat: detect AMap auth failure via OnMapErrorListener, show key input overlay"
    This reverts commit 15025094f2685445356b3058261fe0b9879a7f18.

--- app/src/main/java/com/sinus/pinmap/PinmapApplication.kt
+++ app/src/main/java/com/sinus/pinmap/PinmapApplication.kt
@@ -1,19 +1,13 @@
 package com.sinus.pinmap
 
 import android.app.Application
-import android.util.Log
 import com.amap.api.maps.MapsInitializer
-import com.sinus.pinmap.ui.utils.AuthState
 
 class PinmapApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        AuthState.init(this)
-        val savedKey = AuthState.getSavedKey()
-        val apiKey = savedKey ?: BuildConfig.MAPS_API_KEY
-        MapsInitializer.setApiKey(apiKey)
-        Log.d("KEY", "amap key: $apiKey")
+        MapsInitializer.setApiKey(BuildConfig.MAPS_API_KEY)
         MapsInitializer.updatePrivacyShow(this, true, true)
         MapsInitializer.updatePrivacyAgree(this, true)
         System.setProperty("amap.sdk.update.enable", "false")
